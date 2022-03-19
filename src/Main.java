349507dc975ecc497e06c8d9b6c59e04394ccd1a
Andrzej Nawalaniec <46224560+baqardo@users.noreply.github.com>
2022-03-19 22:58:27 +0100

    Print, write file and binary search

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args){
+        Zad1 zad1 = new Zad1();
+        Zad2 zad2 = new Zad2();
+        Zad3 zad3 = new Zad3();
+
+//        zad1.printFile();
+//        zad2.writeFile();
+
+//        int[] array = { 4, 5, 7, 11, 12, 15, 15, 21, 40, 45 };
+//        int index = zad3.searchIndex(array, 40);
+//        System.out.println("Wynik wyszukiwania: "+index);
+    }
+}
---
+++ src/Zad3.java
@@ -0,0 +1,21 @@
+public class Zad3 {
+    public int searchIndex(int[] array, int value){
+        int index = 0;
+        int limit = array.length - 1;
+
+        while(index <= limit){
+            int point = (int) Math.ceil((index+limit) / 2);
+            int entry  = array[point];
+            if(value > entry){
+                index = point + 1;
+                continue;
+            }
+            if(value < entry){
+                limit = point - 1;
+                continue;
+            }
+            return point;
+        }
+        return -1;
+    }
+}
