9ae29246c3f5fb97dcc869c68383b770ed2ffc87
Andrzej Nawalaniec <46224560+baqardo@users.noreply.github.com>
2022-03-20 15:21:22 +0100

    Print local and global time

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
         Zad2 zad2 = new Zad2();
         Zad3 zad3 = new Zad3();
         Zad4 zad4 = new Zad4();
+        Zad5 zad5 = new Zad5();
 
 //        zad1.printFile();
 //        zad2.writeFile();
@@ -14,6 +15,10 @@ public class Main {
 //        int index = zad3.searchIndex(array, 40);
 //        System.out.println("Wynik wyszukiwania: "+index);
 
-        System.out.println(zad4.crc32("Testowy test"));
+//        System.out.println(zad4.crc32("Testowy test"));
+
+//        zad5.printLocalTime();
+//        zad5.printGlobalTime();
+
     }
 }
