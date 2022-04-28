51881361906613fb126f4468c76072e99fb4f414
Andrzej Nawalaniec <46224560+baqardo@users.noreply.github.com>
2022-04-28 20:58:41 +0200

    Handle user updating his data

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,14 @@
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+  return newObj;
+};
+
 exports.getAllUsers = catchAsync(async (req, res) => {
   const users = await User.find();
 
@@ -18,6 +26,27 @@ exports.createUser = (req, res) => {
     message: 'This route is not yet defined!',
   });
 };
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+  if (req.body.password || req.body.passwordConfirm)
+    return next(
+      new AppError(
+        'This route is not for password updates. Please use /updateMyPassword.',
+        400
+      )
+    );
+
+  const filteredBody = filterObj(req.body, 'name', 'email');
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    status: 'success',
+    data: { user: updatedUser },
+  });
+});
 
 exports.getUser = (req, res) => {
   res.status(500).json({
