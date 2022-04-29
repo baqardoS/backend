57eb1dc07196c71e4af744863b55db04b60a4dc7
Andrzej Nawalaniec <46224560+baqardo@users.noreply.github.com>
2022-04-29 20:04:23 +0200

    Limit number of requests
    Preventing from DoS and Brute Force attacks

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 const cookieParser = require('cookie-parser');
 
 const AppError = require('./utils/appError');
@@ -12,8 +13,14 @@ const app = express();
 //? Middleware
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
 
-app.use(express.json());
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!',
+});
 
+app.use('/api', limiter);
+app.use(express.json());
 app.use(cookieParser());
 
 app.use((req, res, next) => {
