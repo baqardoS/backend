c06d251a8ef4a26f94373a7f404b62b14c0d4bda
Andrzej Nawalaniec <46224560+baqardo@users.noreply.github.com>
2022-04-29 20:35:27 +0200

    Data sanitization
    Preventing from NoSQL query injection and XSS attacks

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 const cookieParser = require('cookie-parser');
 
 const AppError = require('./utils/appError');
@@ -28,6 +30,10 @@ app.use('/api', limiter);
 app.use(express.json({ limit: '10kb' }));
 //? Cookie parser
 app.use(cookieParser());
+//? Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+//? Data sanitization against XSS
+app.use(xss());
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
