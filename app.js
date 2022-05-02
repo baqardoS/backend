d8b4e7e90a027fdc4f162ba9d63ca30c39f84342
Andrzej Nawalaniec <46224560+baqardo@users.noreply.github.com>
2022-05-02 15:40:28 +0200

    Prepare for deployment

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
+const compression = require('compression');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -41,6 +42,7 @@ app.use(mongoSanitize());
 app.use(xss());
 //? Prevent parameter pollution (2 the same parameters)
 app.use(hpp());
+app.use(compression());
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
